33e37f1d16a085df00b0237cbe5f62b8a3494711
MHS <muhamad.salman2001@gmail.com>
2025-09-02 17:42:28 +0200

    feat: added two sum solution

---
+++ hashmap/two_sum.py
@@ -0,0 +1,11 @@
+def two_sum(arr: list[int], target: int) -> list[int]: 
+    num_to_index = {}
+
+    for i, num in enumerate(arr): 
+        complement = target - num
+
+        if complement in num_to_index: 
+            return [num_to_index[complement], i]
+        num_to_index[num] = i
+    return []
+
