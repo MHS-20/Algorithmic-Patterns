65e03ae57ce9f9dda435754c20f981f47bfaf469
MHS <muhamad.salman2001@gmail.com>
2025-09-03 17:04:03 +0200

    feat: added BFS on tree exercises

---
+++ breadth-first-search/tree-level-order-traversal.py
@@ -0,0 +1,24 @@
+from collection import deque
+
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.right = right
+        self.left = left
+
+
+def level_order_traversal(root: Node) -> list[list[int]]:
+    res = []
+    queue = deque([root])
+    while len(queue) > 0:
+        n = len(queue)
+        new_level = []
+        for _ in range(n):
+            node = queue.popleft()
+            new_level.append(node.val)
+            for child in [node.left, node.right]:
+                if child is not None:
+                    queue.append(child)
+        res.append(new_level)
+    return res
