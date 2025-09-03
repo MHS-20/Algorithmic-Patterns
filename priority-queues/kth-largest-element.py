e72ce8eb29b58a3205473c45521dd79befbc81da
MHS <muhamad.salman2001@gmail.com>
2025-09-03 18:56:04 +0200

    feat: added heap ex 2

---
+++ priority-queues/kth-largest-element.py
@@ -0,0 +1,9 @@
+from heapq import heapify, heappop
+
+def find_kth_largest(nums: list[int], k: int) -> int: 
+    nums = [-x for x in nums] ## turn into max heap
+    heapify(nums)
+
+    for _  in range(k-1):
+        heappop(nums)
+    return -nums[0]
