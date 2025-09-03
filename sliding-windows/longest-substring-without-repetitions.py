d8ebea82f0e66fb7122bd231e965c747b2aa3dde
MHS <muhamad.salman2001@gmail.com>
2025-09-03 15:30:53 +0200

    feat: added dynamic sliding window

---
+++ sliding-windows/longest-substring-without-repetitions.py
@@ -0,0 +1,14 @@
+from typing import DefaultDict
+
+
+def longest_substrings_without_repetitions(s: str) -> int : 
+    longest = 0
+    left = 0
+    counter =  dict[str , int] = defaultdict(int)
+    for right in range(len(s)):
+        counter[s[right]] += 1
+        while counter[s[right]] > 1:
+            counter[s[left]] -= 1
+            left += 1
+        longest = max(longest, right - left +  1)
+    return longest
