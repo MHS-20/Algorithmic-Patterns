8c4c9a69af7a0b1658311f449a3c91d5e370bff7
MHS <muhamad.salman2001@gmail.com>
2025-09-03 16:01:41 +0200

    feat: added rotated sorted array problem

---
+++ binary-search/rotated-sorted-array-minimum.py
@@ -0,0 +1,16 @@
+## two sections: those greater than the last element,
+## and those smaller than the last element
+## it's like finding the first true for the second condition
+def find_min_rotated(arr: list[int]) -> int:
+    left, right = 0, len(arr) - 1
+    boundary_index = -1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] <= arr[-1]:
+            boundary_index = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return boundary_index
