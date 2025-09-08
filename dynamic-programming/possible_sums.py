6c2b622b0b4a47ea6f4f2cfec55b886955cacb4b
MHS <muhamad.salman2001@gmail.com>
2025-09-08 17:39:39 +0200

    feat: added possible sum memoization

---
+++ dynamic-programming/possible_sums.py
@@ -0,0 +1,15 @@
+def possible_sums(amount: int, numbers: list[int], memo: dict[int, bool]) -> bool:
+    if amount == 0:
+        return True
+
+    if amount in memo:
+        return memo[amount]
+
+    for n in numbers:
+        if n < amount:
+            if possible_sums(amount - n, numbers, memo):
+                memo[amount] = True
+                return True
+
+    memo[amount] = False
+    return False
