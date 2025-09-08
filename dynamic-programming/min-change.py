376466b7d39339f08e9adbf0d464959445028fa3
MHS <muhamad.salman2001@gmail.com>
2025-09-08 18:19:46 +0200

    feat: added min change with memoizations

---
+++ dynamic-programming/min-change.py
@@ -0,0 +1,18 @@
+def min_change(amount: int, coins: list[int], memo: dict[int, int]) -> int:
+    if amount == 0:
+        return 0
+
+    if amount < 0:
+        return -1
+
+    if amount in memo:
+        return memo[amount]
+
+    min_coins = -1
+    for c in coins:
+        result = min_change(amount - c, coins, memo)
+        if result != -1:
+            min_coins = min(min_coins, result + 1)
+
+    memo[amount] = min_coins
+    return min_coins
