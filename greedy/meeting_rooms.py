dbaab3bda79e2d8adc77070e509a212021b64f4e
MHS <muhamad.salman2001@gmail.com>
2025-09-21 18:39:14 +0200

    feat: added greedy meeting rooms

---
+++ greedy/meeting_rooms.py
@@ -0,0 +1,18 @@
+# given an array of ranges, representing meetings timestamps
+# return how many meeting rooms are needed
+def meeting_rooms(meetings): 
+    data = []
+
+    for start, end in meetings:
+        data.append((start,+1))
+        data.append((end, -1))
+
+    data.sort()
+
+    curr = 0
+    ans = 0
+
+    for _, n in data: 
+        curr += n
+        ans = max(ans, curr)
+    return ans
